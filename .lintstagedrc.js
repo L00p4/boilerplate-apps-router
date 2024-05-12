b5f0553a098cff6fc690a06acc5e2fc9def14af6
Guilherme Marucchi <guilherme.marucchi@sgaseguros.com.br>
2024-05-12 14:43:58 -0300

    initial jest tests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run next:lint`,
-    `npm run lint`
-    // `npm test -- --findRelatedTests ${filenames.join(' ')} --passWithNoTests`
+    `npm run lint`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')} --passWithNoTests`
   ]
 }
